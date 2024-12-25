710e079ce2ac58e6a4cdc13fcbdc8fc2b932db7c
Maxim Efimov <efimov.top.academy@gmail.com>
2024-12-25 21:09:59 +0300

    Добавьте файлы проекта.

---
+++ ThreadingApp/Examples.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadingApp
+{
+    record class Args(int max, int sleep);
+    public static class Examples
+    {
+        public static void ThreadWelcomeExample()
+        {
+            Thread thread = Thread.CurrentThread;
+            thread.Name = "Main";
+
+            Console.WriteLine($"Thread Name: {thread.Name}");
+            Console.WriteLine($"Thread Domain: {Thread.GetDomain().FriendlyName}");
+            Console.WriteLine($"Thread Priority: {thread.Priority}");
+            Console.WriteLine($"Thread State: {thread.ThreadState}");
+
+            Thread threadLoop = new Thread(Loop);
+            Thread threadLoop2 = new Thread(Loop);
+            threadLoop.Start(new Args(10, 500));
+            threadLoop2.Start(new Args(15, 400));
+
+            int id = Thread.CurrentThread.ManagedThreadId;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Thread {id}: {i}");
+                Thread.Sleep(500);
+            }
+        }
+
+        static void Loop(object? argsObj)
+        {
+            if (argsObj is Args args)
+            {
+                int id = Thread.CurrentThread.ManagedThreadId;
+
+                for (int i = 0; i < args.max; i++)
+                {
+                    Console.WriteLine($"Thread {id}: {i}");
+                    Thread.Sleep(args.sleep);
+                }
+            }
+        }
+    }
+}
---
+++ ThreadingApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+
+namespace ThreadingApp
+{
+    internal class Program
+    {
+        static int count = 0;
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Thread thread = new(IncCount);
+                thread.Name = "Thread " + (i + 1).ToString();
+                thread.Start();
+            }
+
+            
+            Console.WriteLine(count);
+        }
+
+        static void IncCount()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                count++;
+                Console.WriteLine($"{Thread.CurrentThread.Name}: {count}");
+                Thread.Sleep(300);
+            }
+                
+        }
+    }
+}
